1bc9cbc433eb4323cb9d01bb06861166f33f0e15
Nina Gorbunova <gnina56565@gmail.com>
2025-12-09 02:07:27 +0400

    Добавить локальный шаблон и правило веток - 2

--- src/main/java/ru/mentee/power/MenteeProgress.java
+++ src/main/java/ru/mentee/power/MenteeProgress.java
@@ -1,12 +1,16 @@
 package ru.mentee.power;
 
-public record MenteeProgress(String menteeName, int sprintNumber, int plannedHoursPerWeek) {
+public record MenteeProgress(
+        String menteeName,
+        int sprintNumber,
+        int plannedHoursPerWeek
+) {
 
     public boolean readyForSprint() {
         return plannedHoursPerWeek >= 3;
     }
 
     public String summary() {
-        return "Sprint %d → %s: planned %d h".formatted(sprintNumber, menteeName, plannedHoursPerWeek);
+        return "Sprint " + sprintNumber + ": " + menteeName + " (planned " + plannedHoursPerWeek + " h)";
     }
 }
