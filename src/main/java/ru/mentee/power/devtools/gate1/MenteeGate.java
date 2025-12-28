7b7817fe4b76188c0f238f853f35affb1637ed17
Gorbunoba Nina <gnina56565@gmail.com>
2025-12-29 02:13:43 +0400

    LiveCoding-1

---
+++ src/main/java/ru/mentee/power/devtools/gate1/MenteeGate.java
@@ -0,0 +1,24 @@
+package ru.mentee.power.devtools.gate1;
+
+public class MenteeGate {
+    public static void printFizzBuzz (int n){
+        for (int i = 1; i <= n; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String[] args){
+        printFizzBuzz(15);
+
+    }
+}
